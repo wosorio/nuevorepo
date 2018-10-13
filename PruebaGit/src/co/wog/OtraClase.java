673e6eee1dd71038c012433c5150e8a1093bea40
William Osorio <wosorio@wog.co>
2018-10-13 15:27:06 -0500

    Tercer Commmit

--- PruebaGit/src/co/wog/OtraClase.java
+++ PruebaGit/src/co/wog/OtraClase.java
@@ -5,6 +5,8 @@ public class OtraClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("holaaaa");
+		System.out.println("holaaaa");
+		System.out.println("holaaaa");
 
 	}
 
